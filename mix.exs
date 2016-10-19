7b1183a6f53ab67ea74689c9867eda51dcf5d233
tom <tommaso@pavese.me>
2016-10-19 03:21:19 +0100

    deps

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule PlugCookieDemo.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :plug, :cowboy]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,9 @@ defmodule PlugCookieDemo.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:plug, "~> 1.2"},
+      {:cowboy, "~> 1.0"}
+    ]
   end
 end
